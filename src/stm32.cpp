3a24dd62168f028b7772898d46bcc0a3993bac54
monolith-kh <kh.cho@monolith.co.kr>
2022-10-05 16:21:21 +0900

    WIP stm32 function

--- src/stm32.cpp
+++ src/stm32.cpp
@@ -13,6 +13,28 @@ uint8_t spiCommand(SPIClass *spi, byte data) {
 }
 
 
+uint8_t* spiStm32Command(SPIClass *spi, uint8_t cmd) {
+    uint8_t ret[7] = {0, };
+    //use it as you would the regular arduino SPI API
+    spi->beginTransaction(SPISettings(VSPI_CLK, MSBFIRST, SPI_MODE0));
+    digitalWrite(VSPI_SS, LOW); //pull SS slow to prep other end for transfer
+    spi->transfer(cmd);
+    spi->transfer(0);
+    digitalWrite(VSPI_SS, HIGH); //pull ss high to signify end of data transfer
+    vTaskDelay(50);
+
+    digitalWrite(VSPI_SS, LOW); //pull SS slow to prep other end for transfer
+    spi->transferBytes(ret, ret, 7);
+    digitalWrite(VSPI_SS, HIGH); //pull ss high to signify end of data transfer
+    spi->endTransaction();
+    vTaskDelay(50);
+
+    log_d("Receive: 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x", ret[0], ret[1], ret[2], ret[3], ret[4], ret[5], ret[6]);
+
+    return ret; 
+}
+
+
 void Stm32Task(void* parameter)
 {
     uint8_t ret = 0;
@@ -37,6 +59,8 @@ void Stm32Task(void* parameter)
         ret = spiCommand(vspi, 0b10101010); // junk data to illustrate usage
         log_d("vspi-2: 0x%x", ret);
         vTaskDelay(100);
+
+        spiStm32Command(vspi, 0x41);
 
         rtlsClient.beginPacket(RTLS_HOST, RTLS_PORT);
         Protocol_position_t protocol = { PK_POSITION_NOTI, CAR_ID, 22, 13, 0, };
