645f36e89538d5a1c53f2a715f196355a46ba564
monolith-kh <kh.cho@monolith.co.kr>
2022-10-21 17:22:31 +0900

    add ota function

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define VERSION "0.1.16"
+#define VERSION "0.1.26"
 
 #ifdef DEV
     #define WIFI_SSID       "RINGGGO-dev"
@@ -10,6 +10,8 @@
     #define GAME_PORT       9998
     #define RTLS_HOST       "oldtrafford.ringggo.981park.net"
     #define RTLS_PORT       9999
+    #define OTA_PORT        9812
+    #define OTA_PASSWORD    "ringggo"
 #else
     #define WIFI_SSID       "RINGGGO"
     #define WIFI_PASSWORD   "mono1010"
@@ -17,6 +19,8 @@
     #define GAME_PORT       9998
     #define RTLS_HOST       "jejupark-rtls.ringggo.981park.com"
     #define RTLS_PORT       9999
+    #define OTA_PORT        9812
+    #define OTA_PASSWORD    "ringggo"
 #endif
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <Arduino.h>
-#include <Wire.h>
+#include <arduino.h>
+#include <wire.h>
+#include <WiFi.h>
+#include <ArduinoOTA.h>
 
 #include "packet.h"
 
@@ -38,7 +40,11 @@ void setup(void)
     carNumber = GetCarNumber();
     log_i("Car Number: %d", carNumber);
 
+#ifdef DEV
+    sprintf(hostName, "CAR_%d(%s)DEV", carNumber, VERSION);
+#else
     sprintf(hostName, "CAR_%d(%s)", carNumber, VERSION);
+#endif
     log_i("hostname: %s", hostName);
 
     WifiInit();
@@ -114,11 +120,47 @@ void setup(void)
         NULL
     );
 
+    ArduinoOTA.setPort(OTA_PORT);
+    ArduinoOTA.setHostname(hostName);
+    ArduinoOTA.setPassword(OTA_PASSWORD);
+
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+            type = "sketch";
+        } else {  // U_FS
+            type = "filesystem";
+        }
+
+        // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+        Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+        }
+    });
+    ArduinoOTA.begin();
+
 }
 
 
 void loop(void)
 {
-
+    ArduinoOTA.handle();
 }
-
